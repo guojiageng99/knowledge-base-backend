d70634d2e937ed07984912a5aff5b3f482041ab6
gjg <guojiageng1024@163.com>
2026-08-06 13:01:27 +0800

    feat: complete graph service and feature acceptance

--- kb-graph/src/main/java/com/knowledge/base/graph/GraphApplication.java
+++ kb-graph/src/main/java/com/knowledge/base/graph/GraphApplication.java
@@ -3,7 +3,13 @@ package com.knowledge.base.graph;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.knowledge.base")
+@SpringBootApplication(
+        scanBasePackages = "com.knowledge.base",
+        exclude = {
+                org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+                org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration.class,
+                org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration.class
+        })
 public class GraphApplication {
 
     public static void main(String[] args) {
